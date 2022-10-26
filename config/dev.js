fb442ee04043649aa2d8a129c9cb5730388ac99c
Yarden Mathieson <yardenm2710@gmail.com>
2022-10-26 14:44:08 +0300

    test

---
+++ config/dev.js
@@ -0,0 +1,7 @@
+module.exports = {
+  // dbURL: 'mongodb://localhost:27017',
+  dbURL: 'mongodb+srv://Yarden:LiXpmQWd42wFEcxL@cluster0.lsmqg.mongodb.net/toy_db?retryWrites=true&w=majority',
+}
+//local env for now
+// '',
+//
---
+++ config/prod.js
@@ -0,0 +1,4 @@
+module.exports = {
+  // dbURL: 'mongodb://localhost:27017',
+  dbURL: 'mongodb+srv://Yarden:LiXpmQWd42wFEcxL@cluster0.lsmqg.mongodb.net/toy_db?retryWrites=true&w=majority',
+}
